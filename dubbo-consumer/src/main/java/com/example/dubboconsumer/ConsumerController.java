5f26713b27dfc88b5d145d83bd8be1f72ed3871d
廖文星 <liaowenxing@nnuo.com>
2024-09-26 18:28:05 +0800

    m

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/ConsumerController.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/ConsumerController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,8 @@ public class ConsumerController {
 
     @Value("${v2java.config:defaultV2java}")
     private String config;
+
+    private String id = "vgjgjgjgj";
 
     @GetMapping("/v2java/hello")
     public Response hello(String name){
@@ -35,5 +38,11 @@ public class ConsumerController {
     public Response config(){
         log.info("config");
         return Response.success(config);
+    }
+
+    @GetMapping("/v2java/sha")
+    public Response sha(){
+        log.info("sha");
+        return Response.success(System.getenv("COMMIT_ID"));
     }
 }
