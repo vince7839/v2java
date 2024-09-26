1feefa21fdaeff3983407e46f98bde6f0699dd25
廖文星 <liaowenxing@nnuo.com>
2024-09-26 15:47:51 +0800

    m

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/ConsumerController.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/ConsumerController.java
@@ -3,15 +3,21 @@ package com.example.dubboconsumer;
 import com.v2java.dto.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class ConsumerController {
 
     @Autowired
     private ConsumerProcessor processor;
+
+    @Value("${v2java.config:defaultV2java}")
+    private String config;
 
     @GetMapping("/v2java/hello")
     public Response hello(String name){
@@ -23,5 +29,11 @@ public class ConsumerController {
     public Response bye(String name){
         log.info("bye");
         return Response.success(processor.bye(name));
+    }
+
+    @GetMapping("/v2java/config")
+    public Response config(){
+        log.info("config");
+        return Response.success(config);
     }
 }
