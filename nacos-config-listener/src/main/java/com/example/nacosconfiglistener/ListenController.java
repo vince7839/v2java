fe7b0f15298e17c1a9d6545b25b10ecf16c8bc21
liaowenxing <vince7839@163.com>
2024-09-29 00:47:48 +0800

    m

---
+++ nacos-config-listener/src/main/java/com/example/nacosconfiglistener/ListenController.java
@@ -0,0 +1,21 @@
+package com.example.nacosconfiglistener;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ListenController {
+
+    @Value("${configKey:ddd}")
+    private String config;
+
+    @GetMapping("/v2java/config")
+    public ResponseEntity test(){
+        return ResponseEntity.ok(config);
+    }
+
+}
---
+++ nacos-config-listener/src/main/java/com/example/nacosconfiglistener/NacosConfigListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosconfiglistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigListenerApplication.class, args);
+    }
+
+}
