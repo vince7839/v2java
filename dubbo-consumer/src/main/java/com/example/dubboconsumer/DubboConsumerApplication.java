2447332e4e98197422b74edd308fb17dbcda9606
kaize <liaowenxing.lwx@alibaba-inc.com>
2026-04-30 10:57:13 +0800

    m

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//启动类
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
